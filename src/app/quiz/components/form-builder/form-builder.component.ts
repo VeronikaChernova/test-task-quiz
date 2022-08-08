79e0a19341af8e6505c2d9a476b598ef9e666db5
Nika <khalipova.veronika@gmail.com>
2022-08-08 16:38:39 +0300

    fix checkboxes

--- src/app/quiz/components/form-builder/form-builder.component.ts
+++ src/app/quiz/components/form-builder/form-builder.component.ts
@@ -15,12 +15,25 @@ export class FormBuilderComponent implements OnInit {
   private dialogRef: any;
   public questions: {options: any, questions: any[]}[] = [];
   pvqForm: FormGroup = this.formBuilder.group({
-    answer0: [''],
+    text0: [''],
+    checkbox1: false,
+    checkbox2: false,
     aliases: this.formBuilder.array([
       this.formBuilder.control('', [Validators.required])
     ])
   });
+  checkboxesForm: FormGroup = this.formBuilder.group({
+    checkbox0: false,
+    checkbox1: false,
+    checkbox2: false,
+    checkbox3: false,
+    checkbox4: false,
+    checkbox5: false
+  });
   questionGroups: FormArray = this.formBuilder.array([
+    this.formBuilder.control('', [Validators.required])
+  ]);
+  checkboxesGroups: FormArray = this.formBuilder.array([
     this.formBuilder.control('', [Validators.required])
   ]);
   private subscriptions = new Subscription();
@@ -41,15 +54,18 @@ export class FormBuilderComponent implements OnInit {
 
     console.log(this.pvqForm);
     this.subscriptions.add(this.dataService.quizQuestionsObs.subscribe(questions => {
-
+      if (!this.questions && questions) {
+        this.questions = questions
+      }
       if (questions) {
+        this.pvqForm.reset();
         this.questionGroups =
           this.formBuilder.array(this.getQuestions(questions).map(question => this.formBuilder.group(question)));
 
         this.pvqForm = this.formBuilder.group({
           questions: this.questionGroups
         });
-        this.cd.markForCheck();
+        // this.cd.markForCheck();
       }
     }));
   }
@@ -62,7 +78,9 @@ export class FormBuilderComponent implements OnInit {
 
         const name = questionsObj.options.type.includes('Paragraph') && i === 0 ? `text${i}` : `checkbox${i}`;
         const value = questionsObj.options.type.includes('Paragraph') && i === 0 ? '' : false;
-        questionControlArray.push({ [name]: [value,  questionsObj.options.isRequired ? Validators.required : null]});
+        if (name.includes('text')) {
+          questionControlArray.push({[name]: [value, questionsObj.options.isRequired ? Validators.required : null], checkboxes: this.checkboxesForm});
+        }
       });
      });
     console.log({questionControlArray});
@@ -92,10 +110,11 @@ export class FormBuilderComponent implements OnInit {
     });
     this.dataService.changeAnswers(result);
 
-    this.router.navigate(['form/answers']);
+    // this.router.navigate(['form/answers']);
   }
 
   getCheckboxName(i: number, questionInfo: any): string {
-    return `checkbox${(i + questionInfo.options.type?.includes('Paragraph') ? 1 : 0)}`;
+    const additionalIndex: number = questionInfo.options.type?.includes('Paragraph') ? 1 : 0;
+    return `checkbox${i + additionalIndex}`;
   }
 }
